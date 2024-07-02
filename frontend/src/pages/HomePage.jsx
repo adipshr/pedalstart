8772562ac23caa217ea315a4001c3387c615bcb0
adipshr <adi.pshr@gmail.com>
2024-07-03 01:50:00 +0530

    Create HomePage component

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import TaskList from "../components/TaskList";
+
+const HomePage = () => {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const response = await axios.get("/api/tasks");
+      setTasks(response.data);
+    };
+
+    fetchTasks();
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-4">Task Management</h1>
+      <TaskList tasks={tasks} />
+    </div>
+  );
+};
+
+export default HomePage;
